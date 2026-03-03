d8d425e9a059ef7907c43a178a43e3fb20cb062b
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-03 17:54:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+module "gh_oidc" {
+  source            = "./modules/wif"
+  pool_id           = "ci-cd"
+  pool_display_name = "GitHub Actions Identity Pool"
+  github_repo       = ""
+  sa_id             = "vpc-provisioner-sa"
+}
+
+# ASSIGN PERMISSIONS (PoLP)
+# 1. Network Admin for VPC/Subnet creation
+resource "google_project_iam_member" "network_admin" {
+  project = var.project_id
+  role    = "roles/compute.networkAdmin"
+  member  = "serviceAccount:${module.gh_oidc.sa_email}"
+}
+
+# 2. Storage Admin for the Terraform State bucket
+resource "google_project_iam_member" "state_admin" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${module.gh_oidc.sa_email}"
+}
+
 #VPC 1
 module "vpc1" {
   source = "./modules/vpc"
