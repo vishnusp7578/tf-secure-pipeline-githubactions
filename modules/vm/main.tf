89b0731444350c711dc610eb1f2519e48ebe700c
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-03 22:47:08 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -27,10 +27,10 @@ resource "google_compute_instance" "vm" {
   EOF
 }
 
-output "external_ip" {
+/* output "external_ip" {
   value = try(google_compute_instance.vm.network_interface[0].access_config[0].nat_ip, null)
 }
 
 output "internal_ip" {
   value = google_compute_instance.vm.network_interface[0].network_ip
-}
+}*/
