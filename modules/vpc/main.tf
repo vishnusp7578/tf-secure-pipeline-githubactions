3e73fb9f1fac6d6a37909cee464f2604d41b6c71
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-03 17:31:17 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,21 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.name
+  auto_create_subnetworks = false
+}
 
+resource "google_compute_subnetwork" "subnets" {
+  for_each = var.subnets
+
+  name          = each.key
+  ip_cidr_range = each.value
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+output "network" {
+  value = google_compute_network.vpc.id
+}
+
+output "subnets" {
+  value = google_compute_subnetwork.subnets
+}
