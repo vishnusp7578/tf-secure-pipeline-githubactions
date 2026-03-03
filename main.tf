b76cecd74afea43b8da5e92161e68cf8701048b5
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-03 18:25:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,41 @@
+# 1. API Enablement (Stage 1)
+resource "google_project_service" "apis" {
+  for_each = toset([
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "sts.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "compute.googleapis.com"
+  ])
+  service            = each.key
+  disable_on_destroy = false
+}
+
+
 module "gh_oidc" {
   source            = "./modules/wif"
   pool_id           = "ci-cd"
   pool_display_name = "GitHub Actions Identity Pool"
   github_repo       = ""
   sa_id             = "vpc-provisioner-sa"
+  depends_on        = [google_project_service.apis]
 } 
 
 # ASSIGN PERMISSIONS (PoLP)
 # 1. Network Admin for VPC/Subnet creation
-resource "google_project_iam_member" "network_admin" {
+resource "google_project_iam_member" "iam_roles" {
+  for_each = toset([
+    "roles/compute.networkAdmin",
+    "roles/storage.objectAdmin",
+    "roles/serviceusage.serviceUsageAdmin"
+  ])
   project = var.project_id
-  role    = "roles/compute.networkAdmin"
+  role    = each.value
   member  = "serviceAccount:${module.gh_oidc.sa_email}"
 }
 
-# 2. Storage Admin for the Terraform State bucket
-resource "google_project_iam_member" "state_admin" {
-  project = var.project_id
-  role    = "roles/storage.objectAdmin"
-  member  = "serviceAccount:${module.gh_oidc.sa_email}"
-}
 
-resource "google_project_iam_member" "service_usage_admin" {
-  project = var.project_id
-  role    = "roles/serviceusage.serviceUsageAdmin"
-  member  = "serviceAccount:${module.gh_oidc.sa_email}"
-}
-
-/* #VPC 1
+#VPC 1
 module "vpc1" {
   source = "./modules/vpc"
 
@@ -38,4 +46,4 @@ module "vpc1" {
     subnet-a = "10.0.1.0/24"
     subnet-b = "10.0.2.0/24"
   }
-} */
+} 
