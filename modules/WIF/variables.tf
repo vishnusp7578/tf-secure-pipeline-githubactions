3dbe48afebcc7da60a6a34ac7b5f8009bb3dc65c
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-03 23:19:43 +0530

    Update variables.tf

--- modules/WIF/variables.tf
+++ modules/WIF/variables.tf
@@ -2,3 +2,4 @@ variable "pool_id" { type = string }
 variable "pool_display_name" { type = string }
 variable "github_repo" { type = string }
 variable "sa_id" { type = string }
+variable "project_id" { type = string }
