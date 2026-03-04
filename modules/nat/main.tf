700a5fe703cd8b0351ced8e79998909a2d3a154e
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-04 10:06:59 +0530

    Create main.tf

---
+++ modules/nat/main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_router" "vpc1_router" {
+  name    = "vpc1-router"
+  network = module.vpc1.network
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "vpc1_nat" {
+  name                       = "vpc1-nat"
+  router                     = google_compute_router.vpc1_router.name
+  region                     = var.region
+  nat_ip_allocate_option      = "AUTO_ONLY" # Automatically allocate external IPs
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                     = module.vpc1.subnets["subnet-b"]
+    source_ip_ranges_to_nat  = ["ALL_IP_RANGES"]
+  }
+}
