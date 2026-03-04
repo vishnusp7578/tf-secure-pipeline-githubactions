9a4a84a0b7cab8988859cabc1aadfa2eb4061265
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-03-04 10:22:30 +0530

    Update main.tf

--- modules/nat/main.tf
+++ modules/nat/main.tf
@@ -11,7 +11,7 @@ resource "google_compute_router_nat" "vpc1_nat" {
   nat_ip_allocate_option      = "AUTO_ONLY" # Automatically allocate external IPs
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork {
-    name                     = module.vpc1.subnets["subnet-b"]
+    name                     = var.subnet_name
     source_ip_ranges_to_nat  = ["ALL_IP_RANGES"]
   }
 }
